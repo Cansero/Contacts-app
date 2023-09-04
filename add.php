fb935f0372f86bbea55b9cb90722eb3d301f86c8
Cansero <ivanmena0011@gmail.com>
2023-09-04 17:36:45 -0400

    Bug fix

--- add.php
+++ add.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <form action="add.php" method="post">
                 <?php if ($error):?>
                     <p><?= $error ?></p>
-                <? endif ?>
+                <?php endif ?>
                 <div class="formitem">
                     <label for="name">Name:</label>
                     <input type="text" id="name" name="name">
