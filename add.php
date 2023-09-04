8290b15455201da82a3ad0a6072ae97f994f4e4f
Cansero <ivanmena0011@gmail.com>
2023-09-04 17:35:34 -0400

    Data validation

--- add.php
+++ add.php
@@ -1,14 +1,24 @@
 <?php
 require "database.php";
 
+$error =null;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
+    if (empty($_POST["name"]) || empty($_POST["number"])) {
+        $error = "Please fill all the fields";
+    } else if (strlen($_POST["number"]) < 9) {
+        $error = "Phone number must be at least 9 characters";
+    } else {
+        $name = $_POST["name"];
     $number = $_POST["number"];
 
-    $statament = $conn->prepare("INSERT INTO contacts (name, number) VALUES ('$name', '$number')");
+    $statament = $conn->prepare("INSERT INTO contacts (name, number) VALUES (:name, :number)");
+    $statament->bindParam(":name", $_POST["name"]);
+    $statament->bindParam(":number", $_POST["number"]);
     $statament->execute();
 
     header("Location: index.php");
+    }
 }
 ?>
 
@@ -36,6 +46,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="add-contact">
             <div class="txt">Add new contact</div>
             <form action="add.php" method="post">
+                <?php if ($error):?>
+                    <p><?= $error ?></p>
+                <? endif ?>
                 <div class="formitem">
                     <label for="name">Name:</label>
                     <input type="text" id="name" name="name">
--- database.php
+++ database.php
@@ -7,9 +7,6 @@ $password = "";
 
 try {
     $conn = new PDO("mysql:hosrt=$host;dbname=$database", $user, $password);
-    // foreach ($conn->query("SHOW DATABASES") as $row) {
-    //     print_r($row);
-    // }
 } catch (PDOException $e) {
     die("PDO Connection Error: " . $e->getMessage());
 }
